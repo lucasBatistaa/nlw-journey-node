a2c11b8d92c0000e961c7ac4d2b259c62ef31a24
lucasBatistaa <lucas.lucas2004.30@gmail.com>
2024-07-12 23:07:58 -0300

    init second class

--- src/routes/create-trip.ts
+++ src/routes/create-trip.ts
@@ -2,26 +2,23 @@ import { FastifyInstance } from "fastify";
 import { ZodTypeProvider } from "fastify-type-provider-zod";
 import { z } from 'zod';
 import { prisma } from "../lib/prisma";
-import dayjs from "dayjs";
-import localizedFormat from 'dayjs/plugin/localizedFormat';
-import 'dayjs/locale/pt-br';
 import nodemailer from 'nodemailer'
 import { getMailClient } from "../lib/mail";
-
-dayjs.locale('pt-br')
-dayjs.extend(localizedFormat)
+import { dayjs } from '../lib/dayjs'
 
 export async function createTrip(app: FastifyInstance) {
-    app.withTypeProvider<ZodTypeProvider>().post('/trips', {
-        schema: {
-            body: z.object({
-                destination: z.string().min(4),
-                starts_at: z.coerce.date(),
-                ends_at: z.coerce.date(),
-                owner_name: z.string(),
-                owner_email: z.string().email(),
-                emails_to_invite: z.array(z.string().email()),
-            })
+    app.withTypeProvider<ZodTypeProvider>().post(
+        '/trips', 
+        {
+            schema: {
+                body: z.object({
+                    destination: z.string().min(4),
+                    starts_at: z.coerce.date(),
+                    ends_at: z.coerce.date(),
+                    owner_name: z.string(),
+                    owner_email: z.string().email(),
+                    emails_to_invite: z.array(z.string().email()),
+            }),
         }
     }, async (request) => {
         const { destination, starts_at, ends_at, owner_name, owner_email, emails_to_invite } = request.body
